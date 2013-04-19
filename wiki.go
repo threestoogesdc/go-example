1b996aa416820e6612a0da68ac9917787dc84f0f
Angel Flores <angel.r.flores1@gmail.com>
2013-04-19 11:58:14 -0400

    added edit and save handlers for wiki app, along with another page test.txt usage: http://localhost:8080/view/test where actions can be view, edit, save

--- wiki.go
+++ wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "fmt"
+  "html/template"
   "io/ioutil"
   "net/http"
 )
@@ -25,15 +25,44 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+  t, _ := template.ParseFiles("views/" + tmpl + ".html")
+  t.Execute(w, p)
+}
+
 const lenPath = len("/view/")
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
   title := r.URL.Path[lenPath:]
-  p, _ := loadPage(title)
-  fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+  p, error := loadPage(title)
+  if error != nil {
+    http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+    return
+  }
+  renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[lenPath:]
+  p, err := loadPage(title)
+  if err != nil {
+    p = &Page{Title: title}
+  }
+
+  renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[lenPath:]
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  p.save()
+  http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
 func main() {
   http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHandler)
   http.ListenAndServe(":8080", nil)
 }
