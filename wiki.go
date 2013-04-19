5923a8c0ad82b97744ee3a13b8f43ac90cdbb917
Angel Flores <angel.r.flores1@gmail.com>
2013-04-19 11:27:00 -0400

    web server using http package, usage: http://localhost:8080/monkeys

--- wiki.go
+++ wiki.go
@@ -2,40 +2,14 @@ package main
 
 import (
   "fmt"
-  "io/ioutil"
+  "net/http"
 )
 
-type Page struct {
-  Title string
-  Body []byte
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-  p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-  p1.save()
-  p2, _ := loadPage("TestPage")
-  fmt.Println(string(p2.Body))
-}
-
-/**
- * method name save that takes as its receiver p, a pointer to Page
- * It takes no parameters and returns value of type error
- */
-func (p *Page) save() error {
-  filename := p.Title + ".txt"
-  return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-/**
- * searches for text file with provided title
- * handles error returned by ioutil.ReadFile
- * returns pointer to a Page and nil for error
- */
-func loadPage(title string) (*Page, error) {
-  filename := title + ".txt"
-  body, err := ioutil.ReadFile(filename)
-  if err != nil {
-    return nil, err
-  }
-  return &Page{Title: title, Body: body}, nil
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
 }
